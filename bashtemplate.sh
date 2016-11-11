c6ec4ac253f91109b906692657cc786fe7976319
paedy <pkz@bluewin.ch>
2016-11-11 18:27:16 +0100

    initial commit
    Initialer commit

---
+++ bashtemplate.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+##############################################################
+#  Script     : template
+#  Author     : paedy
+#  Date       : 11.11.2016
+#  Last Edited: 11.11.2016, paedy
+#  Description: working template 
+##############################################################
+# Purpose:
+# - 
+#
+# Requirements:
+# - 
+#
+# Syntax: template [-h]
+#         template [-m email] [-e email] [-t] [-d]
+#         template [-m email] [-e email] [-t] [-v]
+# Notes:
+# - 
+##############################################################
+
